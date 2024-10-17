cb46d9f7407074e0455435d49c8042d42d8384c4
Lam Duc Trung <vn.trungld@gmail.com>
2024-10-17 07:41:20 +0700

    Update: add laravel to handle BE stuff

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import '../css/app.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import { modalPlugin } from '@kouts/vue-modal';
+import '@kouts/vue-modal/dist/vue-modal.css';
+
+createApp(App).use(modalPlugin).mount('#app')
